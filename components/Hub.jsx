53eeb39da2214560a3ef77ec08961e8468fbcc4c
unknown <joaquinsantarcangelo@gmail.com>
2020-12-31 15:35:48 -0300

    Social icons, hub hero texts and buttons

--- components/Hub.jsx
+++ components/Hub.jsx
@@ -1,19 +1,37 @@
 import React, { useEffect } from "react";
-import { motion } from "framer-motion";
+import { animate, motion, useAnimation } from "framer-motion";
+import { Link } from "react-router-dom";
 
-const Hub = () => {
+const Hub = ({ loading }) => {
   useEffect(() => {
     document.querySelector("#logo").classList.remove("hide");
   }, []);
 
+  const controls = useAnimation();
+
   return (
     <motion.div
       initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
+      animate={loading ? { opacity: 0 } : { opacity: 1 }}
       exit={{ opacity: 0 }}
       className="hub"
     >
-      <div className="wrapper"></div>
+      <div className="wrapper">
+        <div className="title">Julia Tree</div>
+        <div className="subtitle">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quos
+          vitae maiores, ullam unde, praesentium dicta ea corporis labore, quia
+          quidem nam. Tempore dolores illum earum eveniet ipsam hic laboriosam!
+        </div>
+        <div className="buttons">
+          <Link to="/video" style={{ color: "inherit", textDecoration: "none" }}>
+            <div className="button">Watch Video</div>
+          </Link>
+          <Link to="/home" style={{ color: "inherit", textDecoration: "none" }}>
+            <div className="button">Enter Website</div>
+          </Link>
+        </div>
+      </div>
       <div className="background"></div>
     </motion.div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,20 @@
 import Head from "next/head";
+import { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 //Components
 import Menu from "../components/Menu";
 import Navbar from "../components/Navbar";
 import Video from "../components/Video";
+import Home from "../components/Home";
 import Hub from "../components/Hub";
 import Loading from "../components/Loading";
 
-import { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+//Material Icons
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import TwitterIcon from "@material-ui/icons/Twitter";
 
 export default function Index() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -27,9 +32,8 @@ export default function Index() {
     setMenuOpen(!menuOpen);
   };
 
-  // Fake Loading
   useEffect(() => {
-    setTimeout(() => setLoading(false), 2000);
+    setTimeout(() => setLoading(false), 3000);
   }, []);
 
   return (
@@ -44,16 +48,29 @@ export default function Index() {
         <AnimatePresence exitBeforeEnter>
           {menuOpen && <Menu handleMenu={handleMenu} />}
         </AnimatePresence>
-
+        <div className="social-icons">
+          <div className="icon">
+            <FacebookIcon />
+          </div>
+          <div className="icon">
+            <InstagramIcon />
+          </div>
+          <div className="icon">
+            <TwitterIcon />
+          </div>
+        </div>
         <Route
           render={({ location }) => (
             <AnimatePresence exitBeforeEnter initial={false}>
-              {!loading && (
-                <Switch location={location} key={location.pathname}>
-                  <Route path="/" exact component={Hub}></Route>
-                  <Route path="/video" component={Video}></Route>
-                </Switch>
-              )}
+              <Switch location={location} key={location.pathname}>
+                <Route
+                  path="/"
+                  exact
+                  render={() => <Hub loading={loading} />}
+                ></Route>
+                <Route path="/video" component={Video}></Route>
+                <Route path="/home" component={Home}></Route>
+              </Switch>
             </AnimatePresence>
           )}
         />
