fb9e21df063602ec5fc07790e006f6bab6b14b4c
Guidomp11 <ponce.guido@gmail.com>
2021-01-20 22:59:11 -0300

    faq updated

--- components/FAQ.jsx
+++ components/FAQ.jsx
@@ -1,34 +1,50 @@
 import React from "react";
 
+
+const faqs = [
+  {
+    question: 'Que onda bro todo trank?',
+    answer: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+  },
+  {
+    question: 'Es localhost:3000?',
+    answer: 'Si perro'
+  },
+  {
+    question: 'Que onda bro todo trank?',
+    answer: 'Todo trank por suerte'
+  },
+  {
+    question: 'Que onda bro todo trank?',
+    answer: 'Todo trank por suerte'
+  }
+]
+
 const FAQ = () => {
   return (
     <div className="faq">
+      <div className="title">
+          <h2 className="question">Questions</h2>
+          <h3>&</h3>
+          <h2 className="answer">Answers</h2>
+      </div>
       <div className="wrapper">
-        <div className="title"></div>
         <div className="faqs-wrapper">
-          <div id="left" className="side">
-            <div className="item">
-              <div className="question">Que onda bro todo trank?</div>
-              <div className="answer">Que onda bro todo trank?</div>
-            </div>
-            <div className="item">
-              <div className="question">Que onda bro todo trank?</div>
-              <div className="answer">Que onda bro todo trank?</div>
-            </div>
-          </div>
-          <div id="right" className="side">
-            <div className="item">
-              <div className="question">Que onda bro todo trank?</div>
-              <div className="answer">Que onda bro todo trank?</div>
-            </div>
-            <div className="item">
-              <div className="question">Que onda bro todo trank?</div>
-              <div className="answer">Que onda bro todo trank?</div>
-            </div>
-          </div>
+          {
+            faqs.map(faq => (
+              <div className="item">
+                <div className="question">{faq.question}</div>
+                <div className="answer">{faq.answer}</div>
+              </div>
+            ))
+          }
         </div>
       </div>
-      <div className="indicator">{"<1>"}</div>
+      <div className="indicator">
+        <button><i class="arrow left"></i></button>
+        <h2>1</h2>
+        <button><i class="arrow right"></i></button>
+      </div>
     </div>
   );
 };
