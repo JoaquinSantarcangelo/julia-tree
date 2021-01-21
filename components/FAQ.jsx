cc5bda13e5e29aa4a7a5171d8038608330f4cfb9
Guidomp11 <ponce.guido@gmail.com>
2021-01-21 16:00:10 -0300

    faqs layout added

--- components/FAQ.jsx
+++ components/FAQ.jsx
@@ -1,26 +1,72 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
 
-const faqs = [
-  {
-    question: 'Que onda bro todo trank?',
-    answer: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
-  },
-  {
-    question: 'Es localhost:3000?',
-    answer: 'Si perro'
-  },
-  {
-    question: 'Que onda bro todo trank?',
-    answer: 'Todo trank por suerte'
-  },
-  {
-    question: 'Que onda bro todo trank?',
-    answer: 'Todo trank por suerte'
-  }
+const faqsDB = [
+  [
+    {
+      question: 'Que onda bro todo trank?',
+      answer: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+    },
+    {
+      question: 'Es localhost:3000?',
+      answer: 'Si perro'
+    },
+    {
+      question: 'Que onda bro todo trank?',
+      answer: 'Todo trank por suerte'
+    },
+    {
+      question: 'Que onda bro todo trank?',
+      answer: 'Todo trank por suerte'
+    }
+  ],
+  [
+    {
+      question: 'BQue onda bro todo trank?',
+      answer: 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA'
+    },
+    {
+      question: 'BEs localhost:3000?',
+      answer: 'Si perro'
+    },
+    {
+      question: 'BQue onda bro todo trank?',
+      answer: 'Todo trank por suerte'
+    },
+    {
+      question: 'BQue onda bro todo trank?',
+      answer: 'Todo trank por suerte'
+    }
+  ]
 ]
 
 const FAQ = () => {
+
+  const [faqs, handleFAQS] = useState(faqsDB[0]);
+  const [actualpage, handleActualPage] = useState(0);
+
+  useEffect(()=> {
+    const changePage = () => {
+      handleFAQS(faqsDB[actualpage]);
+    }
+    changePage();
+  }, [actualpage]);
+
+  
+  const nextPage = () => {
+    const nextPg = actualpage + 1;
+    if(nextPg <= faqsDB.length){
+      handleActualPage(nextPg);
+    }
+  }
+
+  const prevPage = () => {
+    const prevPg = actualpage - 1;
+    if(prevPg < faqsDB.length){
+      handleActualPage(prevPg);
+    }
+  }
+
   return (
     <div className="faq">
       <div className="title">
@@ -41,9 +87,17 @@ const FAQ = () => {
         </div>
       </div>
       <div className="indicator">
-        <button><i class="arrow left"></i></button>
-        <h2>1</h2>
-        <button><i class="arrow right"></i></button>
+        {(actualpage === 0) ? null : 
+          <button
+            onClick={() => prevPage()}
+          ><i class="arrow left"></i></button>
+        }
+        <h2>{actualpage+1}</h2>
+        {((actualpage+1) === faqsDB.length) ? null : 
+          <button
+            onClick={() => nextPage()}
+          ><i class="arrow right"></i></button>
+        }
       </div>
     </div>
   );
